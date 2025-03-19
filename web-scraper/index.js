d6c0dddcaaadd2fe0f8a83bdbe8acd110c6b63fe
Shashank Shah <nitk.shashankshah@gmail.com>
2025-03-19 20:38:52 +0530

    fix

--- web-scraper/index.js
+++ web-scraper/index.js
@@ -83,9 +83,9 @@ app.get("/load/railway", cors(corsOptions), async (req, res, next) => {
 
 // Handling GET /hello request
 app.get("/load/cnbc", cors(corsOptions), async (req, res, next) => {
-    //var ls = await cnbc_scraper();
-    //var cls = await cnbc_classification();
-    //var cls = await cnbc_industry_classification();
+    var ls = await cnbc_scraper();
+    ls = await cnbc_industry_classification();
+    ls = await cnbc_classification();
     var sentiment = await cnbc_sentiment_analysis()
     res.send(JSON.stringify({}));
 })
@@ -120,10 +120,9 @@ app.get("/load/insider", cors(corsOptions), async (req, res, next) => {
 
 // Handling GET /hello request
 app.get("/load/cnn", cors(corsOptions), async (req, res, next) => {
-    //var ls = await cnn();
-    var cls = {};
-    //var cls = await cnn_industry_classification();
-    //var cls = await cnn_classification();
+    var ls = await cnn();
+    var cls = await cnn_industry_classification();
+    cls = await cnn_classification();
     var cls = await cnn_sentiment_analysis();
     res.send(JSON.stringify(cls));
 })
